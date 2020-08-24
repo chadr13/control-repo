f49305b5cd7a923aba0c182f7d0bc1ae3f55414b
chadr13 <58051949+chadr13@users.noreply.github.com>
2020-08-24 12:36:20 -0700

    Create r10k.pp

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,14 @@
+class profile::r10k {
+  class {'r10k':
+    remote => 'https://github.com/chadr13/control-repo',
+  }
+  class {'r10k::webhook::config:
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class {'r10k::webhook':
+    user  => 'root',
+    group => 'root',
+   }
+}
+    
